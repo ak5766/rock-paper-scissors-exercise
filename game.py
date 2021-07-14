37bc8d859140797492dcf48eac2e312b08e6627b
ak5766 <87001142+ak5766@users.noreply.github.com>
2021-07-13 23:32:37 -0400

    game

---
+++ game.py
@@ -0,0 +1,13 @@
+print ("Rock, Paper, Scissors, Shoot!")
+x = input("Please choose one of the 'rock', 'paper', 'scissors':")
+print(x)
+
+if (x == "rock") or (x == "paper") or (x == "scissors"):
+    print ("Valid")
+    
+
+
+
+
+
+
