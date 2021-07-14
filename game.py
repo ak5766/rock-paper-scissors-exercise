aeab4d3d3ad37e0c26fa7244e0e799690c26bc00
ak5766 <87001142+ak5766@users.noreply.github.com>
2021-07-14 01:48:57 -0400

    last version

--- game.py
+++ game.py
@@ -10,9 +10,28 @@ else:
     exit()
 print("USER CHOSE.",x)
 
-valid_options = ["rock", "paper", "sciccors"]
+valid_options = ["rock", "paper", "scissors"]
 c = (random.choice(valid_options))
 print("Computer Chose.", c)
+
+if(x=="rock") and (c=="scissors"):
+    print( "Rock Wins! Beat em")
+if (x=="paper") and (c=="rock"):
+    print("Paper wins!! You Lost ")
+if (x=="scissors") and (c=="paper"):
+    print("Scicorrs wins!! You got em")
+
+if(c=="rock") and (x=="scissors"):
+    print( "Rock Wins! Comp Wins")
+if (c=="paper") and (x=="rock"):
+    print("Paper wins!! Paper destroys ")
+if (c=="sciccors") and (x=="paper"):
+    print("Scissorrs wins!! Comp win")
+if (x==c):
+    print("Overtime!!")
+    
+
+
 
 
 
