dd0bdbbb699c13d687eee986746b5e9ff11cb9b6
ak5766 <87001142+ak5766@users.noreply.github.com>
2021-07-14 13:43:27 -0400

    passing variables

--- game.py
+++ game.py
@@ -1,8 +1,20 @@
 import random
 
+import os
+from dotenv import load_dotenv 
+
+load_dotenv() #loading content of the file .env file
+
+user_name = os.getenv("USER_NAME")
+
+print(user_name) #reads the variable
+
+# use of random variable for computer
+# establishing the inputs
 print ("Rock, Paper, Scissors, Shoot!")
 x = input("Please choose one of the 'rock', 'paper', 'scissors':")
 print(x)
+# choices for the variable x
 
 if (x == "rock") or (x == "paper") or (x == "scissors"):
     print ("Valid")
@@ -10,26 +22,33 @@ else:
     print("You shall not pass!")
     exit()
 print("USER CHOSE.",x)
-
+#setting the options for computer
 valid_options = ["rock", "paper", "scissors"]
 c = (random.choice(valid_options))
 print("Computer Chose.", c)
 
+# Logic for the game
+# My choices
 if(x=="rock") and (c=="scissors"):
     print( "Rock Wins! Beat em")
 if (x=="paper") and (c=="rock"):
-    print("Paper wins!! You Lost ")
+    print("Paper Wins!! You Lost ")
 if (x=="scissors") and (c=="paper"):
-    print("scissors wins!! You got em")
+    print("Scissors wins!! You got em")
 
+# Choices for computer
 if(c=="rock") and (x=="scissors"):
     print( "Rock Wins! Comp Wins")
 if (c=="paper") and (x=="rock"):
     print("Paper wins!! Paper destroys ")
 if (c=="scissors") and (x=="paper"):
-    print("scissors wins!! Comp win")
+    print("Scissors wins!! Comp win")
 if (x==c):
     print("Overtime!!")
+
+
+
+
 
 
 
