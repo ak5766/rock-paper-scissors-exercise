f5d5c8a9949c62c709849e97b3b0ee8270848673
ak5766 <87001142+ak5766@users.noreply.github.com>
2021-07-14 00:19:14 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+import random
 print ("Rock, Paper, Scissors, Shoot!")
 x = input("Please choose one of the 'rock', 'paper', 'scissors':")
 print(x)
@@ -7,7 +8,16 @@ if (x == "rock") or (x == "paper") or (x == "scissors"):
 else:
     print("You shall not pass!")
     exit()
-    
+print("USER CHOSE.",x)
+
+valid_options = ["rock", "paper", "sciccors"]
+c = (random.choice(valid_options))
+print("Computer Chose.", c)
+
+
+
+
+
 
 
 
